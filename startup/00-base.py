4f37d5df856697582d9e81c6816d7e1a24b53939
XFM Operator <xf04bm@xf04bm-ws1.nsls2.bnl.local>
2025-05-09 10:06:33 -0400

    remove olog configuration as this is not how olog is used on XFM
     * scans are not automatically put into olog

--- startup/00-base.py
+++ startup/00-base.py
@@ -14,7 +14,7 @@ uri = "info.xfm.nsls2.bnl.gov"
 new_md = RedisJSONDict(redis.Redis(uri),prefix="")
 #BEAMLINE_ID = 'xfm'
 
-nslsii.configure_olog(get_ipython().user_ns)
+#nslsii.configure_olog(get_ipython().user_ns)
 
 #Optional: set any metadata that rarely changes.
 #RE.md['beamline_id'] = 'XFM'
